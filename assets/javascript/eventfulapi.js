575f8167f1fbb1079ae4278a4090b2b2c5ea5b31
melissa rice <rice.jean29@gmail.com>
2018-03-17 16:03:16 -0600

    eventfulapiintegration

---
+++ assets/javascript/eventfulapi.js
@@ -0,0 +1,45 @@
+  //bypass cors
+
+  jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+ 
+//eventful api call   
+var apiKey = "&app_key=8BZTgpMGJnMV5sF4";
+var city = "Denver";
+
+$("button").on("click", function(event) {
+    console.log("click worked")
+
+  var queryURL = "http://api.eventful.com/json/events/search?" + apiKey + "&location=" + city;
+  $.ajax({
+    
+      url: queryURL,
+      method: "GET"
+    
+      }).then(function(response) {
+        var data = response;
+        // console.log(JSON.parse(response))
+        var result = JSON.parse(response);
+        // console.log("results" + result);
+        // console.log("data" + data);
+        // console.log("data.length" + data.length);
+     
+        for (j=0; j < data.length; j++) {
+          console.log(result.events.event[j].city_name)
+          console.log(result.events.event[j].latitude)
+          console.log(result.events.event[j].longitude)
+          console.log(result.events.event[j].url)
+          console.log(result.events.event[j].description)
+          console.log(result.events.event[j].title)
+          console.log(result.events.event[j].geocode_type)
+          console.log(result.events.event[j].venue_name)
+          console.log(result.events.event[j].venue_url)
+          console.log(result.events.event[j].postal_code)
+          console.log(result.events.event[j].title)
+        //   console.log(result.events.event[j].image.small.url)
+        }
+      });
+    });  
