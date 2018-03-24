5240e4c426d867c645d0c054f3bdc11b103ec802
Stephanie Dursma <stephaniedursma@Stephanies-MacBook-Pro.local>
2018-03-24 14:09:48 -0600

    added intro page and hide data appearing on search

--- assets/javascript/pages.js
+++ assets/javascript/pages.js
@@ -1,3 +1,25 @@
+$(document).ready(function(){
+    $("#tab").hide();
+    $("#restaurant").hide();
+    $("#dateEvent").hide();
+    
+
+    // $("#search").on("click", function(){
+    //     restaurantsShow();
+    // })
+
+    $("#search").on("click", function restaurantsShow(){
+        $("#intro").hide();
+        $("#tab").show();
+        $("#restaurant").show();
+    });
+
+    $('.tabs').tabs();
+
+
+
+  });
+
 function yelpPage () {
     
 }
