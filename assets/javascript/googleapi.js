e7b177adfbfb671752d4d3847afaf50e15e7c62f
Chad33 <chad.javarush@gmail.com>
2018-03-24 14:15:14 -0600

    changes to google js...close to fin

--- assets/javascript/googleapi.js
+++ assets/javascript/googleapi.js
@@ -1,3 +1,4 @@
+// CORS Bypass
 
 jQuery.ajaxPrefilter(function(options) {
     if (options.crossDomain && jQuery.support.cors) {
@@ -5,8 +6,33 @@ jQuery.ajaxPrefilter(function(options) {
     }
 });
 
-// var cityState = $("#cityState").val().trim();
-// console.log(cityState);
+//Global Variables
+var bizName;
+var G_Lat;
+var G_Long;
+var G_mapCont;
+// var G_placeID;
+
+//On click functions for distance search...
+//NOTE: Google API restricts a maximum radius of 50,000m, hence duplicate distance at #d25 and #d50
+$("#d5").on("click", function(event) {
+    G_distance = 8047;
+    // console.log("distance" + G_distance)
+  });
+  $("#d10").on("click", function(event) {
+    G_distance = 16093;
+    // console.log("distance" + G_distance)
+  });
+  $("#d25").on("click", function(event) {
+    G_distance = 40233;
+    // console.log("distance" + G_distance)
+  });
+  $("#d50").on("click", function(event) {
+    G_distance = 40233;
+    // console.log("distance" + G_distance)
+  });
+
+//On Click Function for City,State Search
 $("#search").on("click", function(event) {
 
     var eventCity =  $("#cityState").val().trim();
@@ -16,10 +42,9 @@ $("#search").on("click", function(event) {
 function displayGoogleShit() {
 
     var photoURL;
-    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurant+" + eventCity + "&key=AIzaSyBn6PzPT-RYHgpew4rKmVGIvENHjQo8-YU";
-    
-    // var postalCode =  $("#zip").val().trim();
-   
+    var apiKey1 = "&key=AIzaSyBn6PzPT-RYHgpew4rKmVGIvENHjQo8-YU"
+    var apiKey2 = "&key=AIzaSyA8-jku2dgDUp9EeXdbeNBksAM4JhOkAxc"
+    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurant+" + eventCity + "&radius=" + G_distance + apiKey2;   
 
     $.ajax({
         url: queryURL,
@@ -28,19 +53,30 @@ function displayGoogleShit() {
 
       .then(function(response) {
         // console.log(info);
-        console.log(response);
+        // console.log(response);
 
         // for (var i =0; i < 10; i++) {
 
-        infoToPage(0);
+        infoToPage(0);        
+
         function infoToPage(i) {
 
            setTimeout(function() {
+            
+            //Google Map Shit
+            G_Lat = parseFloat(response.results[i].geometry.location.lat);
+            G_Long = parseFloat(response.results[i].geometry.location.lng);
+            // console.log(G_Lat);
+            // console.log(G_Long);
+            G_mapCont = "map" + i;
+            
+            var G_maps = $("<div class='G_maps' id='" + G_mapCont +"' >")
+
 
            var photoRef = response.results[i].photos[0].photo_reference;
            var apiKey = "&key=AIzaSyBn6PzPT-RYHgpew4rKmVGIvENHjQo8-YU"       
             
-        //    photoURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoRef + apiKey;
+           photoURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoRef + apiKey2;
 
         //    create event list 
             var container = $("<div class='eventGoogleResults'>");
@@ -50,49 +86,75 @@ function displayGoogleShit() {
             var row4  = $("<tr>");
             var row5  = $("<tr>");
 
+    
             var pic = $("<img>").attr("src", photoURL);
+            
+            bizName = $("<tr class='restTitle'>").text(response.results[i].name );
             // console.log(response.results[i].name);
-            var bizName = $("<tr class='restTitle'>").text(response.results[i].name );
-            // console.log(response.results[i].price_level)
             var pricing = $("<tr>").text("Price Rating: " + response.results[i].price_level);
-            // console.log(response.results[i].formatted_address);
+            // console.log(response.results[i].price_level)
             var bizAddess = $("<tr>").text("Address: " + response.results[i].formatted_address)
-            // console.log(response.results[i].rating);
+            // console.log(response.results[i].formatted_address);
             var googleRating = $("<tr>").text("Google Rating: " + response.results[i].rating);
-            // var googPlaceBlank = response.results[i].photos[0].html_attributions;
-            // $(googPlaceBlank).attr("target", "blank");
-            // var googPlacesSite = $("<tr>").html("Google Places Link: " + googPlaceBlank);
-            var googPlacesSite = $("<tr>").html("Google Places Link: " + response.results[i].photos[0].html_attributions);
+            // console.log(response.results[i].rating);
+            var G_mapUrl ="<a href=https://maps.google.com?saddr=Current+Location&daddr=" + G_Lat + "," + G_Long + " target='_blank'>Directions</a>";
+
+            var googPlacesSite = $("<tr>").html("Google Places: " + response.results[i].photos[0].html_attributions + " | " + G_mapUrl);
             // console.log(response.results[i].photos[0].html_attributions);
+            
 
             row1.append(bizName)
             row2.append(pricing)
             row3.append(googleRating)
             row4.append(bizAddess)
             row5.append(googPlacesSite)
+
+            //dynamically create a div in the Google Body
             var infoGoogDiv = $("<div class='infoGoogDiv'>").append(row1, row2, row3, row4, row5);
-            
-            container.append(pic, infoGoogDiv);
+            var G_row6 = $("<tr class='imgInfo'>");
+            G_row6.append(pic, G_maps);
+            container.append(G_row6, infoGoogDiv);
 
-            $("#google").prepend(container);
+            $("#google").append(container);
+            //call google map function
+            G_initMap(G_Lat,G_Long,G_mapCont);
 
-
+            //safe guards in recursive function
             if (i >= 10) {
                 return;
             }
             
             else {
                 infoToPage(i + 1);
+                
             }
 
-            }, 1000);
-            
+
+            }, 1000);            
 
         }
         
       });
       
     }
+    // End of "displayGoogleShit" function
     displayGoogleShit()
-});
+    
 
+    
+});
+//End of OnClick function
+
+//Google Map Generation function called above when information is appended to the DOM
+function G_initMap(G_Lat,G_Long,G_mapCont) {
+    var map = new google.maps.Map(document.getElementById(G_mapCont), {
+      zoom: 12,
+      center: {lat: G_Lat, lng: G_Long}
+    });
+  
+    var marker = new google.maps.Marker({
+      position: {lat: G_Lat, lng: G_Long},
+      map: map,
+      title: bizName
+    });
+  }
