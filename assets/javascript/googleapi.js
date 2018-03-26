2af3b1c04a31c1af5a5f841faf1d1a76e9e746f0
melissa rice <rice.jean29@gmail.com>
2018-03-25 20:52:01 -0600

    updated error messages

--- assets/javascript/googleapi.js
+++ assets/javascript/googleapi.js
@@ -12,7 +12,8 @@ var G_Lat;
 var G_Long;
 var G_mapCont;
 var G_mapRestName;
-
+var errorMessage;
+var G_distance;
 // var G_placeID;
 
 //On click functions for distance search...
@@ -35,21 +36,8 @@ $("#d5").on("click", function(event) {
   });
 
 
-  if (!G_distance){
-    errorMessage = $("#error").text("Please select a distance and try your search again!");
-    $("#error").show();
-    function verifyDistance() {
-      setTimeout(function(){
-        $("#error").hide();;
-         }, 3000);
-    }
-    verifyDistance();
-  }
-
-
 //On Click Function for City,State Search
 $("#search").on("click", function(event) {
-
     var eventCities =  $("#cityState").val().trim();
 
     $("#google").empty();
@@ -69,103 +57,78 @@ function displayGoogleShit() {
       })
 
       .then(function(response) {
-        // console.log(info);
-        // console.log(response);
-
-        // for (var i =0; i < 10; i++) {
-
-        infoToPage(0);        
-
+        infoToPage(0);
         function infoToPage(i) {
-
            setTimeout(function() {
-            
-            //Google Map Shit
-            G_Lat = parseFloat(response.results[i].geometry.location.lat);
-            G_Long = parseFloat(response.results[i].geometry.location.lng);
-            // console.log(G_Lat);
-            // console.log(G_Long);
-            G_mapCont = "G_maps" + i;
-            
-            var G_maps = $("<div class='G_maps' id='" + G_mapCont +"' >")
+              //Google Map Shit
+              G_Lat = parseFloat(response.results[i].geometry.location.lat);
+              G_Long = parseFloat(response.results[i].geometry.location.lng);
+              // console.log(G_Lat);
+              // console.log(G_Long);
+              G_mapCont = "G_maps" + i;
+              var G_maps = $("<div class='G_maps' id='" + G_mapCont +"' >")
+              var photoRef = response.results[i].photos[0].photo_reference;
+              var apiKey = "&key=AIzaSyBn6PzPT-RYHgpew4rKmVGIvENHjQo8-YU";
+              photoURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoRef + apiKey3;
+              //create event list 
+              var container = $("<div class='eventGoogleResults'>");
+              var row1  = $("<tr>");
+              var row2  = $("<tr>");
+              var row3  = $("<tr>");
+              var row4  = $("<tr>");
+              var row5  = $("<tr>");
+              var pic = $("<img>").attr("src", photoURL);
+              
+              G_mapRestName = response.results[i].name;
+              bizName = $("<tr class='restTitle'>").text(response.results[i].name );
+             
+              var pricing = $("<tr>").text("Price Rating: " + response.results[i].price_level);
+              
+              var bizAddess = $("<tr>").text("Address: " + response.results[i].formatted_address)
+              
+              var googleRating = $("<tr>").text("Google Rating: " + response.results[i].rating);
+              
+              var G_mapUrl ="<a href=https://maps.google.com?saddr=Current+Location&daddr=" + G_Lat + "," + G_Long + " target='_blank'>Directions</a>";
 
+              var googPlacesSite = $("<tr>").html("Google Places: " + response.results[i].photos[0].html_attributions + " | " + G_mapUrl);
+              // console.log(response.results[i].photos[0].html_attributions);
+          
+              row1.append(bizName)
+              row2.append(pricing)
+              row3.append(googleRating)
+              row4.append(bizAddess)
+              row5.append(googPlacesSite)
 
-           var photoRef = response.results[i].photos[0].photo_reference;
-           var apiKey = "&key=AIzaSyBn6PzPT-RYHgpew4rKmVGIvENHjQo8-YU"       
-            
-           photoURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoRef + apiKey3;
+              //dynamically create a div in the Google Body
+              var infoGoogDiv = $("<div class='infoGoogDiv'>").append(row1, row2, row3, row4, row5);
+              var G_row6 = $("<tr class='imgInfo'>");
+              G_row6.append(pic, G_maps);
+              container.append(G_row6, infoGoogDiv);
 
-        //    create event list 
-            var container = $("<div class='eventGoogleResults'>");
-            var row1  = $("<tr>");
-            var row2  = $("<tr>");
-            var row3  = $("<tr>");
-            var row4  = $("<tr>");
-            var row5  = $("<tr>");
+              $("#google").append(container);
+              //call google map function
+              G_initMap(G_Lat,G_Long,G_mapCont);
 
-    
-            var pic = $("<img>").attr("src", photoURL);
-            
-            G_mapRestName = response.results[i].name;
-            bizName = $("<tr class='restTitle'>").text(response.results[i].name );
-            // console.log(response.results[i].name);
-            var pricing = $("<tr>").text("Price Rating: " + response.results[i].price_level);
-            // console.log(response.results[i].price_level)
-            var bizAddess = $("<tr>").text("Address: " + response.results[i].formatted_address)
-            // console.log(response.results[i].formatted_address);
-            var googleRating = $("<tr>").text("Google Rating: " + response.results[i].rating);
-            // console.log(response.results[i].rating);
-            var G_mapUrl ="<a href=https://maps.google.com?saddr=Current+Location&daddr=" + G_Lat + "," + G_Long + " target='_blank'>Directions</a>";
-
-            var googPlacesSite = $("<tr>").html("Google Places: " + response.results[i].photos[0].html_attributions + " | " + G_mapUrl);
-            // console.log(response.results[i].photos[0].html_attributions);
-            
-
-            row1.append(bizName)
-            row2.append(pricing)
-            row3.append(googleRating)
-            row4.append(bizAddess)
-            row5.append(googPlacesSite)
-
-            //dynamically create a div in the Google Body
-            var infoGoogDiv = $("<div class='infoGoogDiv'>").append(row1, row2, row3, row4, row5);
-            var G_row6 = $("<tr class='imgInfo'>");
-            G_row6.append(pic, G_maps);
-            container.append(G_row6, infoGoogDiv);
-
-            $("#google").append(container);
-            //call google map function
-           G_initMap(G_Lat,G_Long,G_mapCont);
-
-            //safe guards in recursive function
-            if (i >= 9) {
-                return;
+              //safe guards in recursive function
+              if (i >= 9) {
+                  return;
+                }
+                else {
+                  infoToPage(i + 1);
+                  // console.log(G_mapRestName);  
+                  }
+              }, 500);      
             }
-            
-            else {
-                infoToPage(i + 1);
-                // console.log(G_mapRestName);
-                
-            }
-
-
-            }, 500);     
-            
-            
-
-        }
-        //end of info to page function
+            //end of info to page function
         
-      });
-      //end of .then Function
+          });
+          //end of .then Function
       
-    }
-    // End of "displayGoogleShit" function
-    displayGoogleShit()
-    // console.log(G_mapRestName)
-
-    
-});
+        }
+        // End of "displayGoogleShit" function
+        displayGoogleShit()
+        // console.log(G_mapRestName)
+    });
 //End of OnClick function
 
 
