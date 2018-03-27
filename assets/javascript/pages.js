ffd5a6a536846474ad6db987cb7ea3a8dc17f56a
Stephanie Dursma <stephaniedursma@Stephanies-MacBook-Pro.local>
2018-03-26 18:53:21 -0600

    added pagination and tabs

--- assets/javascript/pages.js
+++ assets/javascript/pages.js
@@ -1,25 +1,105 @@
 $(document).ready(function(){
     $("#tab").hide();
-    $("#restaurant").hide();
+    $("#googleRest").hide();
     $("#dateEvent").hide();
     
 
-    // $("#search").on("click", function(){
-    //     restaurantsShow();
-    // })
-
-    $("#search").on("click", function restaurantsShow(){
+    $("#search").on("click", function(){
         $("#intro").hide();
         $("#tab").show();
-        $("#restaurant").show();
+        restaurantShow();
     });
 
-    $('.tabs').tabs();
+    $("#restTab").on("click", function(){
+        restaurantShow();
+    });
 
+    function restaurantShow() {
+        $("#googleRest").show();
+        $("#dateEvent").hide()
+    }
 
+    $("#eventTab").on("click", function(){
+        $("#dateEvent").show();
+        $("#googleRest").hide();
+    })
 
-  });
+    // Pagination code
 
-function yelpPage () {
+    // $("#google").DataTable({
+    //     "pagingType": "simple"
+    // });
+
+    $('#google').easyPaginate({
+        paginateElement: 'tr',
+        elementsPerPage: 3,
+        // effect: 'climb'
+    });
+
+    $('#event').easyPaginate({
+        paginateElement: 'tr',
+        elementsPerPage: 3,
+        // effect: 'climb'
+    });
+
+// var list = [];
+// var pageList = [];
+// var currentPage = 1;
+// var numberPerPage = 2;
+// var numberOfPages = 5;
+
+// function newArray() {
+//     var table = $('#google tr').length;
+//     for (var i = 0; i < table; i++){
+//         // var row = $('#google tr').i;
+//         // var obj = {};
+//         // obj.push(row + i: row);
+//         list.push({i: $('#google tr')});
+//     }
+//     console.log(list);
     
-}
+    
+// }
+// newArray();
+
+ 
+
+// function nextPage() {
+//     currentPage += 1;
+//     loadList();
+// }
+
+// function previousPage() {
+//     currentPage -= 1;
+//     loadList();
+// }
+
+// function loadList() {
+//     var begin = ((currentPage - 1) * numberPerPage);
+//     var end = begin + numberPerPage;
+
+//     pageList = list.splice(begin, end);
+//     drawList();
+//     check();
+// }
+    
+// function drawList() {
+//     document.getElementById("list").innerHTML = "";
+//     for (r = 0; r < pageList.length; r++) {
+//         document.getElementById("list").innerHTML += pageList[r] + "<br/>";
+//     }
+// }
+
+// function check() {
+//     document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
+//     document.getElementById("previous").disabled = currentPage == 1 ? true : false;
+//     document.getElementById("first").disabled = currentPage == 1 ? true : false;
+//     document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
+// }
+
+// function load() {
+//     makeList();
+//     loadList();
+// }
+  
+})
