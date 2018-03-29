371d19f299c3a7c7122a5ccd4417b0e975656db9
levi <glendenning_levi@hotmail.com>
2018-03-28 20:52:52 -0600

    added error messages for homepage and added to sytle.css

--- assets/javascript/pages.js
+++ assets/javascript/pages.js
@@ -23,62 +23,196 @@ function yelpPage () {
     messagingSenderId: "1026694530340"
   };
   firebase.initializeApp(config);
-
-  var database = firebase.database();
+   var database = firebase.database();
 
   var email ;
   var password ;
+  var error ;
+  var error2 ;
   // linked to the Sign Up field for the landing page
-  $("#submit").on('click', function(){
-    event.preventDefault()
-     email = $("#email").val().trim();
-     password = $("#password").val().trim();
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      console.log(error);
+  $("#submit").on('click', function(event){
+    event.preventDefault();
+    // console.log("sup");
+    
+      email = $("#email").val().trim();
+      password = $("#password").val().trim();
+      console.log(email);
+      console.log(password);
+      
+      firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function(error) {
+        console.log("then");
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      }
-      console.log(errorCode);
-      console.log(errorMessage);
+      //  var errorCode = error.code;
+          var errorMessage = error.message;
+          // 3/27 cant get the errors to show for create user email and password
+          // console.log(error);
+          // console.log(errorMessage);
+        console.log("Welcome");
+        $("#welcome").text("Welcome to Dat[R]");
+        welcome();
+         
+           
+      }).catch(function(error) { 
+        console.log(error);
+        error = error.code;
+
+        console.log(error);
+
+        if (error === "auth/weak-password" || error === 400){
+         $("#noSignInPassword").show();
+         $("#noSignInPassword").text("Please enter in a Password 6 characters or longer!");
+         
+         noSignInEmail();
+       } 
+       if ( password === "" ){
+         $("#noSignInPassword").show();
+         $("#noSignInPassword").text("Please enter in a Password 6 characters or longer!");
+         
+         noSignInEmail();
+       } 
+       if (error === "auth/invalid-email" || email === "") {
+         $("#noSignInEmail").text('The email address is badly formatted. Make sure you have @ or .com');
+         $("#noSignInEmail").show();
+       signInEmail();
+ 
+        }
+        if (error === "auth/email-already-in-use") {
+          $("#noSignInEmail").text('The email already in use, Please pick another one!');
+          $("#noSignInEmail").show();
+        signInEmail();
+  
+         }
+      })
+      // console.log(errorCode);
+      // console.log(errorMessage);
       // ...
       database.ref().push({
         email: email,
         password: password,
       }); 
     });
+
     
-    console.log(email);
-    console.log(password);
+    // console.log(email);
+    // console.log(password);
 
   
-  })
   // linked to the Sign In field for the landing page
   $("#signIn").on('click', function(event){
     console.log('hi mom');
-    event.preventDefault();
-      email = $("#signInemail").val().trim();
-      password = $("#signInpassword").val().trim();
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      console.log(error);
-       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+    var user = firebase.UserInfo;
+    // var credential = result.credential;
+     console.log(user);
+      console.log(event); 
+      event.preventDefault();
+        email = $("#signInemail").val().trim();
+        password = $("#signInpassword").val().trim();
 
-      console.log(errorCode);
-      console.log(errorMessage);
-      // ...
-      console.log($(this));
-    });
-    console.log($(this));
-    console.log(email);
-    console.log(password);
-    
-  })
-  // database.ref().on("child_added", function(childSnapshot){
-  //   var email = childSnapshot.val().email
-
-  //   var password = childSnapshot.val().password
-  // })
+    if (email === ""){
+      $("#emailErrormessage").text("Asshole");
+      $("#emailErrormessage").show();
+      noshowemail();
+    } 
+    if (password === ""){
+      $("#passWorderror").text("Shit");
+      $("#passWorderror").show();
+      noshowpassword();
+    } 
   
-});
+    
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function(response){
+        console.log('then');
+        if (response){
+          window.location.href = "index.html";
+          
+        }
+  
+        console.log(response);
+        
+      })
+      .catch(function(error) {
+        // console.log(success);
+        console.log(error);
+        
+        
+        if (error.code=== "auth/user-not-found") {
+          $("#emailErrormessage").text('E-mail not found, please verify your E-mail address');
+          $("#emailErrormessage").show();
+          verifyemail();
+
+
+        } else if(error.code=== "auth/wrong-password") {
+      
+          if (($(password).val(""))){
+            $("#passWorderror").text("Shit")
+            $("#passworderror").show();
+            noshowpassword();
+          }
+          if (error.code=== "auth/wrong-password" && (password)){
+            $("#passWorderror").text('Password not found , please verify Password');
+            $("#passWorderror").show();
+              verifypassword();
+          }
+          }
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;        
+      });  
+  });
+})
+
+
+  function verifyemail(){
+    setTimeout(function(){
+      $("#emailErrormessage").hide();  
+      
+    }, 3000);
+  }
+  function verifypassword(){
+    setTimeout(function(){
+      $("#passWorderror").hide();  
+      
+    }, 3000);
+  }
+  function noshowemail(){
+    setTimeout(function(){
+      $("#emailErrormessage").hide();  
+      
+    }, 3000);
+  }
+  function noshowpassword(){
+    setTimeout(function(){
+      $("#passWorderror").hide();  
+      
+    }, 3000);
+  }
+  function noEmail(){
+    setTimeout(function(){
+      $("#noSignInEmail").hide(); 
+
+    }, 3000);
+  }
+  function noSignInEmail(){
+    setTimeout(function(){
+      $("#noSignInPassword").hide(); 
+
+    }, 3000);
+  }
+  function signInEmail(){
+    setTimeout(function(){
+      $("#noSignInEmail").hide(); 
+
+    }, 3000);
+  }
+  function welcome(){
+    setTimeout(function(){
+      window.location.href = "index.html";
+
+    }, 3000);
+  }
